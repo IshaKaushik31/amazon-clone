b6fea4d5668f920b5e15a760a0d33f52ec7ec639
ishakaushik <ishakaushik3104@gmail.com>
2026-04-18 01:39:21 +0530

    create order using backend

--- javascript/checkout/paymentSummary.js
+++ javascript/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import{cart} from '../../data/cart.js';
 import{deliveryOptions,getDeliveryCost} from'../../data/delivery.js';
 import{products,getProduct} from'../../data/products.js';
 import{formatCurrency} from'../utils/money.js';
+import{orders,addOrder} from '../../data/orders.js';
 export function renderPaymentSummary(){
   let totalPrice=0;
   let deliveryPrice=0;
@@ -56,12 +57,36 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(orderTotal)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
   
   `;
   document.querySelector('.js-payment-summary').innerHTML=html;
+  
+  document.querySelector('.js-place-order').addEventListener('click',async()=>{
+    try{
+    const response=await fetch('https://supersimplebackend.dev/orders',{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+
+      },
+      body:JSON.stringify({
+        cart:cart
+      })
+    });
+    const order= await  response.json(); // we use await here because converting our response to json takes time
+    addOrder(order);
+    console.log(order);
+  }
+  catch{
+    console.log('OOPS!Error occured');
+  }
+  window.location.href='orders.html';
+
+  });
+  //here we are trying to make our place order button function by making it interact with the backend using fetvh(). we have created a POST request to our backend.
   
 
 }
